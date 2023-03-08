85586a8086dbc4c335f871ff3211539644825197
absolutelynoot <isalsamudra@hotmail.com>
2023-03-08 14:45:56 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -4,11 +4,23 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+/* import the fontawesome core */
+import { library } from '@fortawesome/fontawesome-svg-core'
+
+/* import font awesome icon component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+/* import specific icons */
+import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
+
 import './assets/main.css'
+
+/* add icons to the library */
+library.add(faUserSecret)
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
-
+app.component('font-awesome-icon', FontAwesomeIcon)
 app.mount('#app')
